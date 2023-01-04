0ec7a3f98c86148e9bc8b54bb446008b530c846d
ondrej <ondrej@sanctusmedia.com>
2023-01-04 15:05:50 +0100

    Initial Commit

---
+++ swift-ui-basics/swift_ui_basicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_basicsApp.swift
+//  swift-ui-basics
+//
+//  Created by Ondrej on 04.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_basicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
