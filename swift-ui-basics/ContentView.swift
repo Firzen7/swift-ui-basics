9c9ff113eff90b47718eae6055f73a1b0a4a093d
ondrej <ondrej@sanctusmedia.com>
2023-01-04 16:01:30 +0100

    Background and state variables

--- swift-ui-basics/ContentView.swift
+++ swift-ui-basics/ContentView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let colours: [Color] = [.blue, .green, .purple]
+    private let colours2: [Color] = [.purple, .green, .blue]
+
+    @State private var screenTapped: Bool = false
+    @State private var offset: CGFloat = -300.0
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            LinearGradient(colors: screenTapped ? colours : colours2,
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            
+            Text("SwiftUI Basics")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .offset(y: offset)
+        }
+        .onTapGesture {
+            // changes screen background on tap
+            screenTapped.toggle()
+            
+            withAnimation(.easeIn(duration: 0.3)) {
+                offset = CGFloat.random(in: 0...300)
+            }
+        }
     }
 }
 
